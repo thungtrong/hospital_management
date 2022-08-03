43860f7c651d643214349b2582bea731cd943631
Tran Hung Trong <tranhungtrong123@gmail.com>
2022-08-03 23:59:43 +0700

    Add libary jdbc

---
+++ sql/insert_some_department.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (id, name, description)
+VALUES (1, 'Accident and Emergency Department', '');
+
+INSERT INTO department (id, name, description)
+VALUES (2, 'Inpatient department', '');
+
+INSERT INTO department (id, name, description)
+VALUES (3, 'Neurology department', '');
+
