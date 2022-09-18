bc73b30ca665a8501de182667f90291e3928f56e
Tran Hung Trong <tranhungtrong123@gmail.com>
2022-09-18 17:37:03 +0700

    Add structure

---
+++ src/main/java/com/hospitalmanagement/repository/AddmissionForm.java
@@ -0,0 +1,7 @@
+package com.hospitalmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddmissionForm extends JpaRepository<AddmissionForm, Long> {
+
+}
