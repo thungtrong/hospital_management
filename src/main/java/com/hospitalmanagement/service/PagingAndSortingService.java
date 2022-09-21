680be8e03501a4e2073c7d6c08943103da9be167
Tran Hung Trong <tranhungtrong123@gmail.com>
2022-09-21 21:31:20 +0700

    Add javadoc comment

--- src/main/java/com/hospitalmanagement/service/PagingAndSortingService.java
+++ src/main/java/com/hospitalmanagement/service/PagingAndSortingService.java
@@ -4,7 +4,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 
 public interface PagingAndSortingService<T, ID> extends CrudService<T, ID>{
+	/**
+	 * Get all entries sorted by {@code sort}
+	 * @param sort
+	 * @return 
+	 */
 	Iterable<T> findAll(Sort sort);
 	
+	/**
+	 * Get {@code size} entries in page number {@code page}
+	 * These entries are sorted by {@code sort} 
+	 * @param page
+	 * @param size
+	 * @param sort
+	 * @return
+	 */
 	Page<T> findAll(int page, int size, Sort sort);
 }
